91500684eff578d29fe5bc4f3a6d4f570c85e574
OKOTB <osamaquotp@gmail.com>
2023-09-02 18:16:46 -0700

    4-source code

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- entery point
+ *
+ * @argc: input argument number
+ * @argv: argument value
+ *
+ * Return: nothing
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) <= 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+}
