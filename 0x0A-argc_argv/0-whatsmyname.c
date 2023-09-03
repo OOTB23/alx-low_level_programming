01c4965e4b1ef18c426a5483a79708a02a872c54
OKOTB <osamaquotp@gmail.com>
2023-09-02 17:12:06 -0700

    0-source code

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- entery point
+ *
+ * @argc: input argument number
+ * @argc: argument
+ *
+ * Return: nothing
+ */
+int main (int argc, char **argv)
+{
+	(void)argc;
+
+	printf ("%s\n", argv[0]);
+	return 0;
+}
