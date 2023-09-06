1729982014d2957c8667acdac8c5790128ead47e
OKOTB <osamaquotp@gmail.com>
2023-09-06 16:40:25 -0700

    0-source code

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - function to creates an array to size of char.
+ * @c: The character to intialize arry with
+ * @size: size of array
+ * Return: array pointer
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(sizeof(size));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		p[0] = c;
+		return (p);
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+project about malloc ,dynamic memory
