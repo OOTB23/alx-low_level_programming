f1734652ff364ac53497c6648cb07411c393c594
OKOTB <osamaquotp@gmail.com>
2023-09-12 19:56:19 -0700

    2-source code

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog- function to print struct type dog
+ *
+ * @d:dog struct
+ *
+ * Description:function to print dog struct
+ *
+ * Return: nothing
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
