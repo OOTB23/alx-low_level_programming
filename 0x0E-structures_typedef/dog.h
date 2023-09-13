83023191a99891ea62b56d2f1a6299676ac95109
OKOTB <osamaquotp@gmail.com>
2023-09-12 19:19:10 -0700

    dog struct source code

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog- struct for dog data
+ *
+ * @name:dog name
+ * @age: dog age
+ * @owner: owner name
+ *
+ * Description: define dog ownership
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
